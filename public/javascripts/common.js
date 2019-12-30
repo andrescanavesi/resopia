8f04c941b62ef5c75bf492dc67851fd370bda77a
Andres Canavesi <andres.canavesi@gmail.com>
2019-12-30 00:03:55 -0300

    load giphy on click

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -50,3 +50,12 @@ function loadYoutubeVideo() {
   videoWrapper.html(`<iframe allowfullscreen frameborder="0" class="embed-responsive-item" src="${videoWrapper.data('yt-url')}"></iframe>`);
   videoWrapper.addClass('embed-responsive embed-responsive-4by3');
 }
+
+/**
+ * Dynamically inject the giphy iframe to avoid loading uncessary stuff during page loading
+ */
+function loadGiphy() {
+  const wrapper = $('#giphyWrapper');
+  wrapper.html('<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/3o7TKr54I53mBu4qLm" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>');
+  wrapper.removeClass('text-center');
+}
