3c5eb641a2c4d797261854249d76894d41d6e61e
Andres Canavesi <andres.canavesi@gmail.com>
2022-01-15 11:07:37 -0300

    push engage method

--- utils/push_engage_helper.js
+++ utils/push_engage_helper.js
@@ -3,17 +3,39 @@ const { Logger } = require('./Logger');
 
 const log = new Logger('push_engage_helper');
 
-module.exports.send = async function (params) {
+const sendPush = async function (recipe) {
+  if (!process.env.RESOPIA_PUSH_ENGAGE_API_KEY) throw new Error('No RESOPIA_PUSH_ENGAGE_API_KEY env var provided');
+  const baseUrl = 'https://api.pushengage.com/apiv1/notifications';
+  const params = {
+    title: recipe.title,
+    message: recipe.description,
+    url: recipe.url,
+    imageUrl: recipe.featured_image_url,
+  };
   log.info(`sending push ${JSON.stringify(params)}`);
-  // curl -X POST -H "api_key: <your_pushengage_api_key>" -H "Content-Type: application/x-www-form-urlencoded" -d 'notification_title=this is title of notification&notification_message=this is message of notification&notification_url=http://www.example.com&image_url=your_image_path/image.png' "https://api.pushengage.com/apiv1/notifications"
-
-  const res = await fetch('https://api.pushengage.com/apiv1/notifications',
+  const bodyForm = `notification_type=draft&notification_title=${params.title}&notification_message=${params.message}&notification_url=${params.url}&image_url=${params.imageUrl}`;
+  const url = baseUrl;
+  const res = await fetch(url,
     {
       method: 'POST',
-      api_key: process.env.RESOPIA_PUSH_ENGAGE_API_KEY
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
+        api_key: process.env.RESOPIA_PUSH_ENGAGE_API_KEY,
       },
+      body: bodyForm,
     });
   const body = await res.text();
+
+  if (res.status >= 300) throw new Error(`error sending push notification status code ${res.status}`);
+  log.info(`notification sent ${body}`);
+  let obj;
+  try {
+    obj = JSON.parse(body);
+  } catch (e) {
+    log.error(e);
+    throw new Error(`error parsing push notification response ${e.message}`);
+  }
+  if (obj.success !== true && obj.success !== 'true') throw new Error(`error sending push notification ${body}`);
 };
+
+module.exports.send = sendPush;
